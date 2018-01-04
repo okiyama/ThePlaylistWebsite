fa2166b4fbbf27796421ff29ae4d302edb1dcfdb
okiyama <lamabuny@gmail.com>
2018-01-04 05:46:38 +0000

    Functional but ugly

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+
+<a href="./vip/">Stream</a>
+<a href="./mp3/">Download</a>
+
+
+<form action="index.php" method="post">
+Youtube URL: <input type="text" name="url"><br />
+<input type="submit" value="Download to server">
+</form>
+
+<?php
+if(isset($_POST["url"])) {
+	$cmd = "youtube-dl -i --extract-audio --audio-format mp3 --output \"/var/www/html/mp3/%(title)s.%(ext)s\" " . escapeshellarg($_POST["url"]);
+	echo "<br />Downloading video now, maybe this will get a progress bar later";
+	echo $cmd;
+	shell_exec($cmd);
+	echo "<br />Finished downloading, updating list of streamable files.";
+	shell_exec("python /var/www/html/vip/genRoster.py");
+	echo "<br />All done!";
+}
+?>
+
+</body>
+</html>
--- vip/genRoster.py
+++ vip/genRoster.py
@@ -3,12 +3,11 @@ from xml.sax.saxutils import escape
 
 xmlFile = '<?xml version="1.0" encoding="UTF-8"?>\n<playlist version="1" xmlns="http://xspf.org/ns/0">\n<trackList>\n'
 for mp3 in sorted(os.listdir("/var/www/html/mp3")):
-    escaped = escape(mp3)
     escapedNoExtension = escape(os.path.splitext(mp3)[0])
     xmlFile += '<track>\n'
     xmlFile += '<creator></creator>\n'
     xmlFile += '<title>' + escapedNoExtension + '</title>\n'
-    xmlFile += '<location>../mp3/' + escaped + '</location>\n'
+    xmlFile += '<location>../mp3/' + mp3 + '</location>\n'
     xmlFile += '</track>\n'
 
 xmlFile += '</trackList>\n</playlist>'
