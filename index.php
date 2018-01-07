9fba61637651ed70d8bfb2eaa6056a253ed294bd
okiyama <lamabuny@gmail.com>
2018-01-07 01:11:40 +0000

    Download now reports progress in real-time to client

--- index.php
+++ index.php
@@ -12,13 +12,36 @@ Youtube URL: <input type="text" name="url"><br />
 
 <?php
 if(isset($_POST["url"])) {
-	$cmd = "youtube-dl -i --extract-audio --audio-format mp3 --output \"/var/www/html/mp3/%(title)s.%(ext)s\" " . escapeshellarg($_POST["url"]);
-	echo "<br />Downloading video now, maybe this will get a progress bar later";
+	echo "Downloading video now\n";
+	flush_to_browser();
 	#echo "<br />Command: " . $cmd;
-	exec($cmd);
-	echo "<br />Finished downloading, updating list of streamable files.";
+	$cmd = "youtube-dl -i --extract-audio --audio-format mp3 --output \"/var/www/html/mp3/%(title)s.%(ext)s\" " . escapeshellarg($_POST["url"]);
+
+	$descriptorspec = array(
+	   0 => array("pipe", "r"),   // stdin is a pipe that the child will read from
+	   1 => array("pipe", "w"),   // stdout is a pipe that the child will write to
+	   2 => array("pipe", "w")    // stderr is a pipe that the child will write to
+	);
+	$process = proc_open($cmd, $descriptorspec, $pipes, realpath('./'), array());
+	echo "<pre>";
+	if (is_resource($process)) {
+	    while ($s = fgets($pipes[1])) {
+		print $s;
+		flush_to_browser();
+	    }
+	}
+	echo "</pre>";
+	echo "Finished downloading, updating list of streamable files.\n";
+	flush_to_browser();
 	exec("python /var/www/html/vip/genRoster.py");
-	echo "<br />All done!";
+	echo "All done!\n";
+}
+
+function flush_to_browser() {
+	echo '<div style="display:none">';
+	echo str_pad('',4096)."\n";
+	echo "</div>";
+	flush();
 }
 ?>
 
